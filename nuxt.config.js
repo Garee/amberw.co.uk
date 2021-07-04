38df56281ffbd4b819dff458ddeb7fdc484da4e1
Gary Blackwood <gary@garyblackwood.co.uk>
2021-07-04 15:30:11 +0100

    hide blog & projects

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,11 +43,11 @@ export default {
       {
         rel: 'preload',
         as: 'style',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+        href: 'https://fonts.googleapis.com/css2?family=Otomanopee+One:wght@400;600;700&display=swap'
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Otomanopee+One:wght@400;600;700&display=swap',
         media: 'print',
         onload: `this.media='all'`
       },
@@ -56,7 +56,7 @@ export default {
     noscript: [
       {
         innerHTML:
-          '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">'
+          '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Otomanopee+One:wght@400;600;700&display=swap">'
       }
     ],
     __dangerouslyDisableSanitizers: ['noscript']
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,20 @@
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">Amber Wilson</h1>
-      <h2 class="subtitle text-center">Theoretical Physicist (in training) 👩‍🔬</h2>
+      <h2 class="subtitle text-center">Student 👩‍🎓, Vegan 🥕, Scientist 👩‍🔬</h2>
+      <p class="text-center max-w-2xl">
+        Hey 👋 I'm a second year MSc Theoretical Physics student at The University of Glasgow. I owe
+        <a href="https://animalcrossing.fandom.com/wiki/Tom_Nook">Tom Nook</a> a million bells 🔔😅, but I plan on
+        paying him back when I discover the next subatomic particle. Please
+        <a href="mailto:amber@amberw.co.uk">send me an e-mail</a> if you would like to chat. 😊
+      </p>
     </section>
 
+    <!--
     <section class="mt-8">
       <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Blog</h3>
       <posts post-type="blog" />
     </section>
+    -->
   </main>
 </template>
